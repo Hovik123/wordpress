1ac802c2a9ceb413da906ab0d9fac6351a893913
hovhannes <hovhannespost@gmail.com>
2016-10-25 01:23:03 +0400

    add angular 2 in to wo

---
+++ wp-content/themes/artsasha/wp-app.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ Template Name: App
+ */
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Wpng2Test</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/favicon.ico">
+    <script type="text/javascript" async src="<?php echo get_template_directory_uri(); ?>/dist/inline.js"></script>
+    <script type="text/javascript" async
+            src="<?php echo get_template_directory_uri(); ?>/dist/styles.bundle.js"></script>
+    <script type="text/javascript" async src="<?php echo get_template_directory_uri(); ?>/dist/main.bundle.js"></script>
+</head>
+<body>
+<app-root>Loading...</app-root>
+</body>
+</html>
